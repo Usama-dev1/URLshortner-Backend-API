a931e8c2e9770ed68264966157f7aa658ea55698
Usama-dev1 <raja.usamah@gmail.com>
2024-08-08 13:18:48 +0500

    added cors

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const mongoose = require("mongoose");
 const urlRouter = require("./routes/urlRoutes");
 const { fetchController } = require("./controllers/urlController");
 const path = require("path");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(
+  cors({
+    origin: "https://url-shortner-backend-omega.vercel.app/",
+  })
+);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
