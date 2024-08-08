bc43eba62155eaa88c8a63788988fc564ea29bcc
Usama-dev1 <raja.usamah@gmail.com>
2024-08-08 13:25:51 +0500

    added cors updated

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(
-  cors({
-    origin: "https://url-shortner-backend-omega.vercel.app/",
-  })
+  cors()
 );
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
